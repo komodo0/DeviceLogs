eefd9282187c079c56fcf4cb7f0aaa06a47844d3
Котельников Иван <i.kotelnikov@ritm.local>
2016-10-26 17:29:37 +0300

    Initial commit

---
+++ func.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: i.kotelnikov
+ * Date: 26.10.2016
+ * Time: 14:50
+ */
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+    <script src="jquery-2.2.4/jquery.min.js"></script>
+
+    <script src="moment/moment-with-locales.min.js"></script>
+
+    <script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
+
+    <script src="bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+
+    <link href="bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/i18n/defaults-ru_RU.min.js"></script>
+
+    <script src="winmarkltd-BootstrapFormHelpers-d4201db/js/bootstrap-formhelpers-phone.js"></script>
+
+    <script src="winmarkltd-BootstrapFormHelpers-d4201db/js/bootstrap-formhelpers-phone.format.js"></script>
+
+    <script src="bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js"></script>
+
+    <title>
+        DeviceLogs
+    </title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+
+    </div>
+</div>
+</body>
+</html>
